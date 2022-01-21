4e6cf53e54ddcb3ec5b15e3cc6e459a0bfc96d9b
sinamics <bernt.christian.egeland@gmail.com>
2022-01-21 13:59:44 +0000

    build

--- docker/bin/entrypoint.sh
+++ docker/bin/entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 
 sleep 3
 # Make sure we set the webport stored in the database, if user uses another port than standard 80
-/app/uavcast/bin/build/uav_main -p >> waitforit.txt
+# /app/uavcast/bin/build/uav_main -p >> waitforit.txt
 
 # sleep 3
 # systemctl restart uavcast-web >> waitforit.txt
