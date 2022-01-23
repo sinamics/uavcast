dbe753a65a20d92fb21d6b8eb761708f7dbf29ff
Bernt Christian Egeland <cbegelan@gmail.com>
2022-01-23 18:54:57 +0000

    vscode

--- docker/bin/dev-build.sh
+++ docker/bin/dev-build.sh
@@ -10,27 +10,17 @@ APPROOT="/app/uavcast"
 # install global dependencies
 npm i concurrently ts-node-dev typescript -g
 
-sudo apt-get install -y libsqlite3-dev
+sudo apt-get install -y libjsoncpp-dev \
+                        libsqlite3-dev \
+                        libv4l-dev \
+                        libcap2-bin
 
-sudo apt-get install -y jq dnsutils usb-modeswitch modemmanager openvpn \
-network-manager-openvpn dh-autoreconf gstreamer-1.0 gstreamer1.0-tools gstreamer1.0-omx \
-gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly gstreamer1.0-plugins-bad python-pip \
-libgstreamer-plugins-base1.0* libgstreamer1.0-dev libgstrtspserver-1.0-dev gstreamer1.0-plugins-base-apps \
-python3-gi python3 python3-dev python3-pip python3-gst-1.0 network-manager
-
-sudo apt-get install -y libv4l-dev && sudo ln -s /usr/include/libv4l1-videodev.h   /usr/include/linux/videodev.h
-sudo apt-get install -y libjsoncpp-dev
-
-# Allow non sudo user to host on port 80
-sudo apt-get install -y libcap2-bin
+sudo ln -s /usr/include/libv4l1-videodev.h   /usr/include/linux/videodev.h
 sudo setcap cap_net_bind_service=+ep `readlink -f \`which node\``
 
 # fetch translations
 # https://github.com/UAVmatrix/uavcast-pro-translations
 git submodule update --init --recursive
-
-# Build binaries
-cd /app/uavcast/bin && make
 
 # Generate UAVcast.service file
 UAVCAST=$Systemd/"uavcast.service"
@@ -117,5 +107,5 @@ sudo touch /var/run/docker.sock
 sudo chmod 666 /var/run/docker.sock
 sudo chown uavcast:docker /var/run/docker.sock
 
-## vscode
-code --install-extension ${APPROOT}/.vscode/extensions.json
+# Build binaries
+cd /app/uavcast/bin && make
