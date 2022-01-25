78abc415c5216509de44e8e43cfed565a61c978b
Bernt Christian Egeland <cbegelan@gmail.com>
2022-01-25 19:46:45 +0100

    user 1000

--- backend/src/graphql-types/MyContext.ts
+++ backend/src/graphql-types/MyContext.ts
@@ -1,10 +1,5 @@
 import { Request, Response } from 'express';
 
-declare module 'express-session' {
-  interface SessionData {
-    userId: any;
-  }
-}
 export interface MyContext {
   req: Request;
   res: Response;
