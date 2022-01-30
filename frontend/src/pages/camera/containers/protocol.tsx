66147feb43a1c425f80fa9bba47734e70c8e83e2
Bernt Christian Egeland <bernt.christian.egeland@gmail.com>
2022-01-30 16:29:14 +0000

    url

--- frontend/src/pages/camera/containers/protocol.tsx
+++ frontend/src/pages/camera/containers/protocol.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Dropdown, Grid, Header } from 'semantic-ui-react';
 import { useCameraDataQuery, useUpdateCameraMutation } from '../../../graphql/generated/dist';
+import { extractHostname } from '../../../utils/apolloClient';
 
 export const protocolArr = [
   { key: 'tcp', value: 'tcp', text: 'TCP', disabled: true },
@@ -17,7 +18,7 @@ const CameraProtocol = () => {
   };
 
   const { protocol } = cameraData?.database || {};
-
+  const url = extractHostname(window.location.href);
   return (
     <Grid doubling padded columns={2}>
       <Grid.Column>
@@ -38,9 +39,8 @@ const CameraProtocol = () => {
           options={protocolArr}
           placeholder='Camera Protocol'
         />
-        {/* <Header as='p' subheader='access video stream by using rtsp://<rpi_ip>:8554/uavcast' /> */}
         <span className='themeText'>
-          access video stream by using <span style={{ color: 'yellow' }}>rtsp://rpi_ip:8554/uavcast</span>
+          access video stream by using <span style={{ color: 'yellow' }}>rtsp://{url}:8554/uavcast</span>
         </span>
         {/* <Label basic>Default selectOnNavigation</Label> */}
       </Grid.Column>
