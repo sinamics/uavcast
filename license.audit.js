6ad811af47a4a2f111f148ff18c381266b995846
Bernt Christian Egeland <bernt.christian.egeland@gmail.com>
2022-05-24 19:13:03 +0100

    license audit

--- license.audit.js
+++ license.audit.js
@@ -1,12 +1,16 @@
-const backendPackageJSON = require('./backend/package.json');
-// const frontendPackageJSON = require('./frontend/package.json');
+/**
+ * Run this script from the root of the repo you want to inspect. It's assumed
+ * that there's a `./node_modules` and `./package.json` where this script runs.
+ */
+
+const module = `backend`; // or frontend
+
+const packageJSON = require(`./${module}/package.json`);
 
 // Get all the top-level dependencies for the current project.
 const dependencies = {
-  ...backendPackageJSON.dependencies,
-  ...backendPackageJSON.devDependencies
-  //   ...frontendPackageJSON.dependencies,
-  //   ...frontendPackageJSON.devDependencies
+  ...packageJSON.dependencies,
+  ...packageJSON.devDependencies
 };
 
 // Get the names of every dependency.
@@ -14,7 +18,7 @@ const dependencyNames = Object.keys(dependencies);
 
 // Create a key => value of depenencyName => { license, whateverElse };
 const licenses = dependencyNames.reduce((memo, dependencyName) => {
-  const dependencyDirectory = `./backend/node_modules/${dependencyName}`;
+  const dependencyDirectory = `./${module}/${dependencyName}`;
   const dependencyPackageJSON = require(`${dependencyDirectory}/package.json`);
   const license = dependencyPackageJSON.license;
 
