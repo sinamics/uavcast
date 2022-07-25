e3c8926bd5b777ed8bea918331b4500c22707920
Bernt Christian Egeland <bernt.christian.egeland@gmail.com>
2022-07-25 18:23:04 +0000

    added scroll to bottom for logviewer

--- frontend/src/pages/logviewer/containers/dockerLog.tsx
+++ frontend/src/pages/logviewer/containers/dockerLog.tsx
@@ -1,11 +1,13 @@
 import React, { useEffect } from 'react';
 import { Label } from 'semantic-ui-react';
 import { useGetDockerLogMutation } from '../../../graphql/generated/dist';
+//@ts-ignore
+import ScrollToBottom from 'react-scroll-to-bottom';
 
 const DockerLog = () => {
   //get server logs
   const [fetchLog, { data, loading, error }] = useGetDockerLogMutation({
-    variables: { properties: { minutes: 240, limit: 35 } },
+    variables: { properties: { minutes: 240 } },
     fetchPolicy: 'network-only'
   });
 
@@ -17,21 +19,23 @@ const DockerLog = () => {
   if (error) return <div>Error</div>;
 
   return (
-    <div className='themeBg themeText'>
-      {data?.getDockerLog.file?.map((log: any, idx: number) => {
-        // check if first word is error or failed
-        const errorWords = ['failed', 'error'];
-        const isError = new RegExp(errorWords.join('|')).test(log?.message.toLowerCase());
-        return (
-          <div key={idx} style={{ fontSize: '16px' }}>
-            <Label color={`${isError ? 'red' : 'green'}`} horizontal>
-              {log.timestamp}
-            </Label>
-            <span style={{ background: `${isError ? '#a1131366' : ''}` }}>{`${log.message}`}</span>
-          </div>
-        );
-      })}
-    </div>
+    <ScrollToBottom>
+      <div className='themeBg themeText logContainer'>
+        {data?.getDockerLog.file?.map((log: any, idx: number) => {
+          // check if first word is error or failed
+          const errorWords = ['failed', 'error'];
+          const isError = new RegExp(errorWords.join('|')).test(log?.message.toLowerCase());
+          return (
+            <div key={idx} style={{ fontSize: '16px' }}>
+              <Label color={`${isError ? 'red' : 'green'}`} horizontal>
+                {log.timestamp}
+              </Label>
+              <span style={{ background: `${isError ? '#a1131366' : ''}` }}>{`${log.message}`}</span>
+            </div>
+          );
+        })}
+      </div>
+    </ScrollToBottom>
   );
 };
 
